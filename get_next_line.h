065add8c69aeb45cf49814381eef2e6ef80a84c9
teathas <asmaber722@gmail.com>
2024-11-17 16:28:55 +0100

    get_next_line first commit

---
+++ get_next_line.h
@@ -0,0 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char *s1, char const *s2);
+int	ft_strlen(const char *s);
+int	ft_strlcat(char *dst, const char *src, int size);
+int	ft_strlcpy(char *dst, const char *src, int dstsize);
---
+++ get_next_line_utils.c
@@ -0,0 +1,66 @@
+#include "get_next_line.h"
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_strlcpy(char *dst, const char *src, int dstsize)
+{
+	int	i;
+
+	i = 0;
+	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+int	ft_strlcat(char *dst, const char *src, int size)
+{
+	int	dst_l;
+	int	src_l;
+	int	i;
+
+	if (dst == NULL || size == 0)
+		return (ft_strlen(src));
+	dst_l = ft_strlen(dst);
+	src_l = ft_strlen(src);
+	i = 0;
+	if (dst_l >= size)
+		return (src_l + size);
+	while (src[i] && i < size - dst_l - 1)
+	{
+		dst[dst_l + i] = src[i];
+		i++;
+	}
+	dst[dst_l + i] = '\0';
+	return (dst_l + src_l);
+}
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+	char    *str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	return (str);
+}
