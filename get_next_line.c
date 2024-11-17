a81c8d7847692dedfaf340f33454d8b6c3acb058
teathas <asmaber722@gmail.com>
2024-11-17 22:13:44 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,6 @@ char	*get_line(char *str)
 	int	size;
 	char	*line;
 
-	if (!str)
-		return (NULL);
 	i = 0;
 	size = 0;
 	while (str[i] && str[i] != '\n')
@@ -41,17 +39,38 @@ char	*get_remain(char	*str)
 	j = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
+	if (str[i] && str[i] == '\n')
+		i++;
 	if (!str[i])
+	{
+		free(str);
 		return (NULL);
+	}
 	size = ft_strlen(&str[i]) + 1;
 	remain = malloc(sizeof(char) * size);
 	if (!remain)
 		return (NULL);
 	while (str[i])
 		remain[j++] = str[i++];
-	free(str);
 	remain[j] = '\0';
+	free(str);
 	return (remain);
+}
+
+int	nl_check(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 int	read_file(int fd, char **buffer)
@@ -63,18 +82,21 @@ int	read_file(int fd, char **buffer)
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return (0);
-	while (readed > 0)
+	while (!nl_check(*buffer) && readed > 0)
 	{
 		readed = read(fd, buff, BUFFER_SIZE);
 		if (readed == -1)
 		{
 			free(buff);
+			free(*buffer);
 			return (0);
 		}
-		if (readed > 0)
+		else if (readed > 0)
 		{
 			buff[readed] = '\0';
 			*buffer = ft_strjoin(*buffer, buff);
+			if (nl_check(*buffer))
+				break;
 		}
 	}
 	free(buff);
@@ -87,9 +109,16 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 
 	line = NULL;
+	if (fd < 0)
+		return (NULL);
 	if (!read_file(fd, &buffer))
 		return (NULL);
-	line = get_line(buffer);
-	buffer = get_remain(buffer);
+	if (buffer)
+	{
+		line = get_line(buffer);
+		buffer = get_remain(buffer);
+		if (!buffer)
+			free(buffer);
+	}
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -73,6 +73,8 @@ char    *ft_strjoin(char *s1, char const *s2)
 {
 	char    *str;
 
+	if (!s1 && !s2)
+		return (NULL);
 	if (!s1)
 		return (ft_strdup(s2));
 	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
